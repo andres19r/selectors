d053e80d596387e63546e0af6f5bf542f684b1c0
Andres Rivero <andres19rivero@gmail.com>
2022-08-25 06:31:27 -0400

    get the countries by the region in the form

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,27 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountrySmall } from '../../interfaces/countries.interface';
 import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
-  styleUrls: ['./selector-page.component.css']
+  styleUrls: ['./selector-page.component.css'],
 })
 export class SelectorPageComponent implements OnInit {
   myForm: FormGroup = this.fb.group({
-    region: ['', Validators.required]
-  })
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+  });
 
   // fill selectors
-  regions: string[] = []
+  regions: string[] = [];
+  countries: CountrySmall[] = [];
 
-  constructor(private fb: FormBuilder, private countriesService: CountriesService) { }
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
 
   ngOnInit(): void {
-    this.regions = this.countriesService.regions
+    this.regions = this.countriesService.regions;
+
+    // When the regions changes
+    this.myForm
+      .get('region')
+      ?.valueChanges.subscribe((region) => {
+        this.countriesService.getCountriesByRegion(region)
+          .subscribe(countries => {
+            console.log(countries)
+            this.countries = countries
+          })
+      });
   }
 
   save() {
-    console.log(this.myForm.value)
+    console.log(this.myForm.value);
   }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,15 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CountrySmall } from '../interfaces/countries.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private baseUrl: string = 'https://restcountries.com/v2'
   private _regions: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
 
   get regions(): string[] {
     return [...this._regions];
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCountriesByRegion(region: string): Observable<CountrySmall[]> {
+    const url: string = `${this.baseUrl}/region/${region}?fields=alpha3Code,name`
+    return this.http.get<CountrySmall[]>(url)
+  }
 }
